ff7f43e09a85202cf8dc7cab8242fe82af6a4dac
Yuriy Pavlyshak <yuriy@whereby.com>
2022-07-26 16:33:48 +0200

    Add swift package manager demo app
    This is the copy of the existing cocoapods-based demo app (with all the cocoapods setup removed) that integrates WherebySDK via swift package manager.

---
+++ Demo-SwiftPM/WherebySDKDemo/SceneDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.swift
+//  WherebySDKDemo
+//
+//  Created by Yuriy Pavlyshak on 06/07/2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+}
+
